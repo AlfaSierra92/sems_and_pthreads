2fec5271192cfc0687fc5ef2f2adb4e74df5b294
AlfaSierra92 <solida.antonio@gmail.com>
2023-05-16 12:09:15 +0200

    Esercizio 5 w/ sem

---
+++ rt/es5.c
@@ -0,0 +1,106 @@
+/*
+Esercizio 5.
+Sembra funzionare tutto a prima botta. La consegna richiedeva di fare solo la versione con pthread ma ho usato i semafori.
+*/
+#define USA_SEM
+
+#include <stdio.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define ELETTORI 11 //deve essere un numero dispari!
+
+void pausetta(void){
+    struct timespec t;
+    t.tv_sec = 0;
+    t.tv_nsec = (rand()%10+1)*1000000;
+    nanosleep(&t,NULL);
+}
+
+#ifdef USA_SEM
+struct urna_t{
+    sem_t mutex; //mutex
+    sem_t ho_votato; //elettore aspetta risultati
+    sem_t scrutinio; //lo scrutatore aspetta per scrutare
+
+    int n_voti0, n_voti1;
+    int votanti;
+} urna;
+
+void init_urna(struct urna_t *u){
+    sem_init(&u->mutex, 0, 1);
+    sem_init(&u->ho_votato, 0, 0);
+    sem_init(&u->scrutinio, 0, 0);
+
+    u->n_voti0 = u->n_voti1 = u->votanti = 0;
+}
+
+void vota(int v){
+    sem_wait(&urna.mutex);
+    switch (v){
+    case 0:
+        printf("Voto 0\n");
+        urna.n_voti0++;
+        printf("N voti 0: %d\n",urna.n_voti0);
+        break;
+    
+    case 1:
+        printf("Voto 1\n");
+        urna.n_voti1++;
+        printf("N voti 1: %d\n",urna.n_voti1);
+        break;
+    }
+    urna.votanti++;
+
+    if (urna.n_voti0 > ELETTORI/2 || urna.n_voti1 > ELETTORI/2){
+        sem_post(&urna.mutex);
+        for(int i=0; i<ELETTORI; i++) sem_post(&urna.ho_votato);
+    } else {
+        sem_post(&urna.mutex);
+        sem_wait(&urna.ho_votato);
+    }
+
+}
+
+int risultato(){
+    if (urna.n_voti0 > urna.n_voti1) return 0;
+    else return 1;
+}
+
+//codice dato dall'esercizio
+void *thread(void *arg){
+    int voto = rand()%2;
+    pthread_t thread_id = pthread_self();
+
+    vota(voto);
+    if (voto == risultato()) printf("%lu Ho vinto!\n", thread_id);
+    else printf("%lu Ho perso!\n", thread_id);
+    pthread_exit(0);
+}
+
+#endif
+
+int main(){
+    pthread_attr_t a;
+    pthread_t p;
+
+    init_urna(&urna);
+
+    srand(time(0));
+
+    pthread_attr_init(&a);
+
+    pthread_attr_setdetachstate(&a, PTHREAD_CREATE_DETACHED);
+
+    for (int i=0; i<ELETTORI; i++){
+        pthread_create(&p, &a, thread, NULL);
+    }
+
+    pthread_attr_destroy(&a);
+
+    sleep(5);
+
+    return 0;
+}
