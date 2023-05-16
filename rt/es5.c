670de4099a7538c081e8fc0220d2d1804939846c
AlfaSierra92 <solida.antonio@gmail.com>
2023-05-16 12:24:53 +0200

    Es5 w/ cond; first draft

--- rt/es5.c
+++ rt/es5.c
@@ -3,6 +3,7 @@ Esercizio 5.
 Sembra funzionare tutto a prima botta. La consegna richiedeva di fare solo la versione con pthread ma ho usato i semafori.
 */
 #define USA_SEM
+//#define USA_PTHREAD
 
 #include <stdio.h>
 #include <semaphore.h>
@@ -80,6 +81,76 @@ void *thread(void *arg){
     pthread_exit(0);
 }
 
+#endif
+
+#ifdef USA_PTHREAD
+struct urna_t{
+    pthread_mutex_t mutex; //mutex
+    pthread_cond_t ho_votato; //elettore aspetta risultati
+    pthread_cond_t scrutinio; //lo scrutatore aspetta per scrutare
+
+    int n_voti0, n_voti1;
+    int votanti;
+} urna;
+
+void init_urna(struct urna_t *u){
+    pthread_mutexattr_t m_attr;
+    pthread_condattr_t c_attr;
+    pthread_mutexattr_init(&m_attr);
+    pthread_condattr_init(&c_attr);
+
+    pthread_mutex_init(&u->mutex, &m_attr);
+    pthread_cond_init(&u->ho_votato, &c_att);
+    pthread_cond_init(&u->scrutinio, &c_att);
+
+    pthread_condattr_destroy(&c_attr);
+    pthread_mutexattr_destroy(&m_attr);
+
+    u->n_voti0 = u->n_voti1 = u->votanti = 0;
+}
+
+void vota(int v){
+    sem_wait(&urna.mutex);
+    switch (v){
+    case 0:
+        printf("Voto 0\n");
+        urna.n_voti0++;
+        printf("N voti 0: %d\n",urna.n_voti0);
+        break;
+    
+    case 1:
+        printf("Voto 1\n");
+        urna.n_voti1++;
+        printf("N voti 1: %d\n",urna.n_voti1);
+        break;
+    }
+    urna.votanti++;
+
+    if (urna.n_voti0 > ELETTORI/2 || urna.n_voti1 > ELETTORI/2){
+        sem_post(&urna.mutex);
+        for(int i=0; i<ELETTORI; i++) sem_post(&urna.ho_votato);
+    } else {
+        sem_post(&urna.mutex);
+        sem_wait(&urna.ho_votato);
+    }
+
+}
+
+int risultato(){
+    if (urna.n_voti0 > urna.n_voti1) return 0;
+    else return 1;
+}
+
+//codice dato dall'esercizio
+void *thread(void *arg){
+    int voto = rand()%2;
+    pthread_t thread_id = pthread_self();
+
+    vota(voto);
+    if (voto == risultato()) printf("%lu Ho vinto!\n", thread_id);
+    else printf("%lu Ho perso!\n", thread_id);
+    pthread_exit(0);
+}
 #endif
 
 int main(){
