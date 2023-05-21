37ad6bed8f8e36f366dc9a2d27ec9877e91b50e3
AlfaSierra92 <solida.antonio@gmail.com>
2023-05-21 11:46:34 +0200

    Pasticceria Eta Beta

--- rt/pasticceria.c
+++ rt/pasticceria.c
@@ -7,6 +7,22 @@ volta. La vendita di una torta da parte del commesso coincide con l'acquisto da 
 Il sistema è modellato tramite un thread per il cuoco, uno per il commesso, ed uno per ogni cliente. Il numero di
 clienti non è specificato, e non è importante il loro ordine di accodamento
 */
+/*
+NOTA 1:
+Allo start del programma, quando ovviamente non ci sono torte,
+il cuoco fa il post sul semaforo commesso, semaforo che rimarrà sempre a 1
+fino a quando le torte saranno nuovamente 0.
+In quel momento entrerà nel blocco della if e farà la wait sul semaforo il
+quale non bloccherà; successivamente essendo il numero delle torte negativo,
+non passerà più quella if generando un ciclo che consuma torte negative.
+Per risolvere questo problema, faccio in modo che il commesso all'apertura del negozio,
+dia per scontato che c'è già almeno una torta disponibile alla vendita mediante la variabile
+apertura_negozio che sarà a 0 solo allo start e poi rimarrà sempre settata a 1.
+
+NOTA 2:
+Il cuoco, una volta aperto il negozio, riempe tutta la vetrina di torte ma non aspetta che si svuoti completamente per rimettersi all'opera;
+ogni volta che ne viene venduta una ricomincia a lavorare per riempire immediatamente lo spazio vuoto.
+*/
 
 #define USA_SEM
 //#define USA_PTHREAD
@@ -17,7 +33,7 @@ clienti non è specificato, e non è importante il loro ordine di accodamento
 #include <unistd.h>
 #include <stdlib.h>
 
-#define N 5 //numero massimo di torte invendute
+#define N 50 //numero massimo di torte invendute
 #define CLIENTI 25
 
 void pausetta(void){
@@ -32,11 +48,14 @@ struct pasticceria_t {
     sem_t mutex;
     sem_t cuoco; //serve per bloccare il cuoco se ci sono troppe torte
     sem_t commesso; //serve per bloccare commesso in caso di assenza di torte
-    sem_t cliente, cassa, acquisto; //sync tra commesso e cliente
+    sem_t cliente; //aspetta la disponibilità del commesso
+    sem_t cassa; //blocca il commesso aspettando che "il cliente che paghi"
+    sem_t acquisto; //blocca il cliente aspettando che la transazione sia ok
     //cosa succede se non ci sono torte? Il cliente non viene sbloccato dal
     //commesso!
 
     int torte_invendute;
+    int apertura_negozio;
     //sem_t torte;
 
 } pasticceria;
@@ -50,6 +69,7 @@ void init_pasticceria(struct pasticceria_t *p){
     sem_init(&p->acquisto, 0, 0);
 
     p->torte_invendute = 0;
+    p->apertura_negozio = 0;
 }
 
 void cuoco_inizio_torta(struct pasticceria_t *p){
@@ -65,7 +85,8 @@ void cuoco_fine_torta(struct pasticceria_t *p){
     sem_wait(&p->mutex);
     if (p->torte_invendute == 0){
         p->torte_invendute++;
-        sem_post(&p->commesso); //sblocca il commesso che aspetta almeno una torta
+        if (p->apertura_negozio != 0) //vedi nota 1 in testa
+            sem_post(&p->commesso); //sblocca il commesso che aspetta almeno una torta
     } else p->torte_invendute++;
     printf("Ci sono %d torte in vetrina.\n\n", p->torte_invendute);
     
@@ -79,7 +100,7 @@ void commesso_prendo_torta(struct pasticceria_t *p){
         sem_wait(&p->commesso); //aspetta che sia disp una torta
         sem_wait(&p->mutex);
     }
-
+    p->apertura_negozio = 1;
     sem_post(&p->cliente); //avanti il prossimo cliente!
     sem_post(&p->mutex);
 }
@@ -91,7 +112,7 @@ void commesso_vendo_torta(struct pasticceria_t *p){
     if (p->torte_invendute == N) {
         p->torte_invendute--; //il cuoco, che nel frattempo si era fermato,
         printf("Cuoco al lavorooo!\n"); //viene rimesso al lavoro
-        sem_post(&p->cuoco);
+        sem_post(&p->cuoco); 
     } else {
         p->torte_invendute--;
         //sem_post(&p->cliente);
